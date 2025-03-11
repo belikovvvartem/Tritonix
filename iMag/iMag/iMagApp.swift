ca8ab915fbef667825e30f131cf3d46337454b8b
Artem <beliko.artem43334@icloud.com>
2025-03-11 18:42:29 +0200

    .

---
+++ iMag/iMag/iMagApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMagApp.swift
+//  iMag
+//
+//  Created by Artem Belikov on 05.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct iMagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iMag/iMagTests/iMagTests.swift
@@ -0,0 +1,16 @@
+//
+//  iMagTests.swift
+//  iMagTests
+//
+//  Created by Artem Belikov on 05.03.2025.
+//
+
+import Testing
+
+struct iMagTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
